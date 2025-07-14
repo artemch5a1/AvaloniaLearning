8c9bf33915261b93a9790f6ea8f679a8f9d273f5
praktika <praktika@PRAKTIKA>
2025-07-14 15:58:29 +0300

    implement Splat DI

--- AvaloniaLearning/App.axaml.cs
+++ AvaloniaLearning/App.axaml.cs
@@ -1,13 +1,19 @@
+using System;
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using AvaloniaLearning.NavigationStore;
+using AvaloniaLearning.NavService;
 using AvaloniaLearning.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using Splat.Microsoft.Extensions.DependencyInjection;
 
 namespace AvaloniaLearning
 {
     public partial class App : Application
     {
+        private IServiceProvider? _serviceProvider;
+
         public override void Initialize()
         {
             AvaloniaXamlLoader.Load(this);
@@ -15,6 +21,22 @@ namespace AvaloniaLearning
 
         public override void OnFrameworkInitializationCompleted()
         {
+            ServiceCollection services = new ServiceCollection();
+
+            services.AddSingleton<NavStore>();
+
+            services.AddSingleton<NavigationService>();
+
+            services.AddSingleton<MainWindowViewModel>();
+
+            services.AddTransient<MainPageViewModel>();
+
+            services.AddTransient<StartPageViewModel>();
+
+            services.UseMicrosoftDependencyResolver();
+
+            _serviceProvider = services.BuildServiceProvider();
+
             NavStore navStore = new NavStore();
 
             navStore.Navigate<StartPageViewModel>();
---
+++ AvaloniaLearning/NavService/NavigationService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AvaloniaLearning.NavigationStore;
+using AvaloniaLearning.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AvaloniaLearning.NavService
+{
+    public class NavigationService
+    {
+        private readonly NavStore _navStore;
+        private readonly IServiceProvider _serviceProvider;
+
+        public NavigationService(NavStore navStore, IServiceProvider serviceProvider)
+        {
+            _navStore = navStore;
+            _serviceProvider = serviceProvider;
+        }
+
+        public void Navigate<TViewModel>()
+            where TViewModel : ViewModelBase
+        {
+            ViewModelBase? viewModel = _serviceProvider.GetRequiredService<TViewModel>();
+            _navStore.CurrentViewModel = viewModel;
+        }
+    }
+}
