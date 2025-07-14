e0a693ca78dce39c792f3ce2dbd539c52d433576
praktika <praktika@PRAKTIKA>
2025-07-14 15:29:45 +0300

    Create NavService.cs

---
+++ AvaloniaLearning/NavService/NavService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AvaloniaLearning.NavigationStore;
+using AvaloniaLearning.ViewModel;
+
+namespace AvaloniaLearning.NavService
+{
+    public class NavService<TViewModel>
+        where TViewModel : ViewModelBase
+    {
+        private readonly NavStore _navStore;
+
+        public NavService(NavStore navStore)
+        {
+            _navStore = navStore;
+        }
+    }
+}
