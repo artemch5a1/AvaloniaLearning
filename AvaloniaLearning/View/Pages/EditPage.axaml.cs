9e0152676d5b74dfaa39f177b28487b008053363
Artem <alefa2000@mail.ru>
2025-07-17 21:41:00 +0300

    new view done

---
+++ AvaloniaLearning/View/Pages/EditPage.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace AvaloniaLearning.View.Pages;
+
+public partial class EditPage : UserControl
+{
+    public EditPage()
+    {
+        InitializeComponent();
+    }
+}
