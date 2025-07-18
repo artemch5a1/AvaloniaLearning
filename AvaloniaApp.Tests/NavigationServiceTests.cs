27a4382f999419d8a9874a8515cadb873730ddcd
Artem <alefa2000@mail.ru>
2025-07-18 20:04:32 +0300

    Add Tests for NavService

--- AvaloniaApp.Tests/NavigationServiceTests.cs
+++ AvaloniaApp.Tests/NavigationServiceTests.cs
@@ -1,18 +1,41 @@
-﻿namespace AvaloniaApp.Tests
+﻿using AvaloniaApp.NavigationStore;
+using AvaloniaApp.NavService;
+using AvaloniaApp.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace AvaloniaApp.Tests
 {
     public class NavigationServiceTests
     {
-        
+        private ServiceProvider _serviceProvider;
 
-        NavigationServiceTests() 
+        public NavigationServiceTests()
         {
+            ServiceCollection serviceDescriptors = new ServiceCollection();
 
+            serviceDescriptors.AddTransient<ViewModelBase>();
+
+            serviceDescriptors.AddSingleton<NavigationService>();
+
+            serviceDescriptors.AddSingleton<NavStore>();
+
+            _serviceProvider = serviceDescriptors.BuildServiceProvider();
         }
 
         [Fact]
-        public void Test1()
+        public void Navigate_NavigateToVM_ShouldChangeCurrentVM()
         {
+            NavigationService navigationService =
+                _serviceProvider.GetRequiredService<NavigationService>();
 
+            Assert.NotNull(navigationService);
+
+            navigationService.Navigate<ViewModelBase>();
+
+            NavStore navStore = _serviceProvider.GetRequiredService<NavStore>();
+
+            Assert.True(navStore.CurrentViewModel!.GetType() == typeof(ViewModelBase));
         }
     }
 }
