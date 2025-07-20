a014beb64c908ed7d5e78a075bb9c06821d529ba
Artem <alefa2000@mail.ru>
2025-07-20 18:46:35 +0300

    moving INavigationService

--- AvaloniaLearning/NavService/NavigationService.cs
+++ AvaloniaLearning/NavService/NavigationService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using AvaloniaApp.NavStore;
+using AvaloniaApp.ServiceAbstractions;
 using AvaloniaApp.ViewModel;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
