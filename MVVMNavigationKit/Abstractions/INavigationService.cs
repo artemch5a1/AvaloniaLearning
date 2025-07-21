087fcdff139c84fad728f4c7d5947b708ca47c0f
Artem <alefa2000@mail.ru>
2025-07-21 21:46:14 +0300

    new project

---
+++ MVVMNavigationKit/Abstractions/INavigationService.cs
@@ -0,0 +1,36 @@
+﻿using MvvmNavigationKit.Abstractions.ViewModelBase;
+
+namespace AvaloniaApp.ServiceAbstractions
+{
+    public interface INavigationService
+    {
+        void Navigate<TViewModel>()
+            where TViewModel : ViewModelTemplate;
+
+        void Navigate<TViewModel, TParams>(TParams @params)
+            where TViewModel : ViewModelTemplate;
+
+        public void DestroyAndNavigate<TViewModel>()
+            where TViewModel : ViewModelTemplate;
+
+        public void DestroyAndNavigate<TViewModel, TParams>(TParams @params)
+            where TViewModel : ViewModelTemplate;
+
+        public void NavigateOverlay<TViewModel>(
+            Action<ViewModelTemplate?>? overlayAction = null,
+            Action? onClose = null
+        )
+            where TViewModel : ViewModelTemplate;
+
+        public void NavigateOverlay<TViewModel, TParam>(
+            TParam @params,
+            Action<ViewModelTemplate?>? overlayAction = null,
+            Action? onClose = null
+        )
+            where TViewModel : ViewModelTemplate;
+
+        public void CloseOverlay();
+
+        void NavigateBack();
+    }
+}
---
+++ MVVMNavigationKit/Abstractions/INavigationStore.cs
@@ -0,0 +1,10 @@
+﻿using MvvmNavigationKit.Abstractions.ViewModelBase;
+using System.ComponentModel;
+
+namespace MvvmNavigationKit.Abstractions
+{
+    public interface INavigationStore : INotifyPropertyChanged
+    {
+        ViewModelTemplate? CurrentViewModel { get; set; }
+    }
+}
