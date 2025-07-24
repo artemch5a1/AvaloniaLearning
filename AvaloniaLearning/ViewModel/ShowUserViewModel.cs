929af3cd8202c7ad8c850031357bebe6c5c56792
praktika <praktika@PRAKTIKA>
2025-07-24 15:22:24 +0300

    Update ShowUserViewModel.cs

--- AvaloniaLearning/ViewModel/ShowUserViewModel.cs
+++ AvaloniaLearning/ViewModel/ShowUserViewModel.cs
@@ -1,10 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
+using AvaloniaApp.Models;
+using AvaloniaApp.ServiceAbstractions;
+using CommunityToolkit.Mvvm.ComponentModel;
+using MvvmNavigationKit.Abstractions;
 
 namespace AvaloniaApp.ViewModel
 {
-    public partial class ShowUserViewModel : ViewModelBase { }
+    public partial class ShowUserViewModel : ViewModelBase
+    {
+        private readonly INavigationService _navigationService;
+        private readonly IUserService _userService;
+
+        [ObservableProperty]
+        private User _user = new();
+
+        [ObservableProperty]
+        private string _error = string.Empty;
+
+        private int _idUser;
+
+        public ShowUserViewModel(INavigationService navigationService, IUserService userService)
+        {
+            _navigationService = navigationService;
+            _userService = userService;
+        }
+
+        protected override void InitializeParams<T>(T @params)
+        {
+            _idUser = GetAs<int>(@params);
+            _ = LoadUser();
+        }
+
+        public override void RefreshPage()
+        {
+            _ = LoadUser();
+        }
+
+        private async Task LoadUser()
+        {
+            User? user = _userService.GetUserById(_idUser);
+            if (user != null)
+            {
+                User = user;
+            }
+            await Task.CompletedTask;
+        }
+    }
 }
